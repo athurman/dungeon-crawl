28f9c495b6b8387914b62e8c17b343f2a1bc235b
Amanda Thurman <athurman215@gmail.com>
2013-11-09 17:16:47 -0600

    orc bug fix

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -38,44 +38,6 @@ function clickMoveSpace(e) {
   htmlMoveOrcs();
 }
 
-function htmlMoveDragon() {
-  var $pastDragon = $('#board > div.dragon');
-  $pastDragon.removeClass('dragon');
-  var move = _.range($('div.tile').length);
-  move = _.sample(move);
-  var $dragon = $('#board > div.tile:nth-child(' + move + ')').addClass('dragon');
-  if($dragon.hasClass('wormhole')) {
-    var position = _.range($('div.tile').length);
-    position = _.sample(position);
-    $dragon = $('#board > div.tile:nth-child(' + position + ')').addClass('dragon');
-  }
-  if($dragon.hasClass('hero')){
-    alert('Game Over!');
-  }
-}
-
-function htmlMoveOrcs() {
-  debugger;
-  var $pastOrcs = $('#board > div.orc');
-  $pastOrcs.addClass('pastOrc').removeClass('orc');
-  var $orc;
-  for(var i = 0; i < $('#board > div.pastOrc').length; i++) {
-    var position = _.range($('div.tile').length);
-    position = _.sample(position);
-    $orc = $('#board > div.tile:nth-child(' + position + ')').addClass('orc');
-    if($orc.hasClass('hero')) {
-      var damage = _.sample([1,2,3,4,5]);
-      var num = $('#health-bar > div.health').length;
-      var health = num - damage;
-      htmlUpdateHealth(health);
-    }
-    if($orc.hasClass('wormhole')){
-      console.log('Touched the Wormhole!!');
-    }
-  }
-  $pastOrcs.removeClass('pastOrc');
-}
-
 //  ------------------------------------------------------------------ //
 //  ------------------------------------------------------------------ //
 //  ------------------------------------------------------------------ //
@@ -97,13 +59,6 @@ function htmlAddBoard(game, e){
   addHealth(game);
 }
 
-// function htmlAddHealth(game) {
-//   for(var i = 0; i < game.hero.health; i++){
-//     var $health = $('<div>').addClass('health').attr('data-health-point', [i]);
-//     $('#health-bar').append($health);
-//   }
-// }
-
 function htmlUpdateHealth(num) {
   $('#health-bar > div.health').remove();
   for(var i = 0; i < num; i++){
@@ -112,6 +67,55 @@ function htmlUpdateHealth(num) {
   }
   var $hp = $('#health-bar > h4').text('HP: ' + $('#health-bar > div.health').length + ' pts');
 }
+
+function htmlMoveDragon() {
+  var $pastDragon = $('#board > div.dragon');
+  $pastDragon.removeClass('dragon');
+  var move = _.range($('div.tile').length);
+  move = _.sample(move);
+  var $dragon = $('#board > div.tile:nth-child(' + move + ')').addClass('dragon');
+  if($dragon.hasClass('wormhole')) {
+    var position = _.range($('div.tile').length);
+    position = _.sample(position);
+    $dragon = $('#board > div.tile:nth-child(' + position + ')').addClass('dragon');
+  }
+  if($dragon.hasClass('hero')){
+    alert('Game Over!');
+  }
+}
+
+function htmlMoveOrcs() {
+  var $pastOrcs = $('#board > div.orc');
+  $pastOrcs.addClass('pastOrc').removeClass('orc');
+  var $orc;
+  var positionArray = _.range($('div.tile').length);
+  for(var i = 0; i < $('#board > div.pastOrc').length; i++) {
+    var position = _.sample(positionArray);
+    var index = position;
+    positionArray.splice(index, 1);
+    var $tile = $('#board > div.tile:nth-child(' + position + ')');
+    if(!$tile.hasClass('orc')) {
+      $orc = $tile.addClass('orc');
+      if($orc.hasClass('hero')) {
+        var damage = _.sample([1,2,3,4,5]);
+        var num = $('#health-bar > div.health').length;
+        var health = num - damage;
+        htmlUpdateHealth(health);
+      }
+      if($orc.hasClass('wormhole')){
+        console.log('Touched the Wormhole!!');
+        var move = _.range($('div.tile').length);
+        move = _.sample(position);
+        $orc = $('#board > div.tile:nth-child(' + position + ')').addClass('orc');
+      }
+    } else {
+      console.log('Lost an orc');
+    }
+  }
+  $pastOrcs.removeClass('pastOrc');
+}
+
+// Orcs disappear currently... the dragon eats them.  That is all.
 
 //  ------------------------------------------------------------------ //
 //  ------------------------------------------------------------------ //
