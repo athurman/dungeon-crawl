273c7526e10626cbf60ab1ab02c128afa8afb3fd
Amanda Thurman <athurman215@gmail.com>
2013-11-09 16:35:35 -0600

    characters move

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -5,7 +5,7 @@ $(document).ready(initialize);
 function initialize(){
   $(document).foundation();
   $('#form').on('submit', submitNewGame);
-  $('#board').click('click', clickMoveSpace);
+  $('#board').on('click', '.tile', clickMoveSpace);
 }
 
 
@@ -24,7 +24,56 @@ function submitNewGame(e) {
 }
 
 function clickMoveSpace(e) {
+  var $tile = $(this);
+  var $pastHero = $('#board > div.hero');
+  $pastHero.removeClass('hero');
+  if($tile.hasClass('wormhole')) {
+    var position = _.range($('div.tile').length);
+    position = _.sample(position);
+    $tile = $('#board > div.tile:nth-child(' + position + ')').addClass('hero');
+  } else {
+    $tile.addClass('hero');
+  }
+  htmlMoveDragon();
+  htmlMoveOrcs();
+}
 
+function htmlMoveDragon() {
+  var $pastDragon = $('#board > div.dragon');
+  $pastDragon.removeClass('dragon');
+  var move = _.range($('div.tile').length);
+  move = _.sample(move);
+  var $dragon = $('#board > div.tile:nth-child(' + move + ')').addClass('dragon');
+  if($dragon.hasClass('wormhole')) {
+    var position = _.range($('div.tile').length);
+    position = _.sample(position);
+    $dragon = $('#board > div.tile:nth-child(' + position + ')').addClass('dragon');
+  }
+  if($dragon.hasClass('hero')){
+    alert('Game Over!');
+  }
+}
+
+function htmlMoveOrcs() {
+  debugger;
+  var $pastOrcs = $('#board > div.orc');
+  $pastOrcs.addClass('pastOrc').removeClass('orc');
+  var $orc;
+  for(var i = 0; i < $('#board > div.pastOrc').length; i++) {
+    var position = _.range($('div.tile').length);
+    position = _.sample(position);
+    $orc = $('#board > div.tile:nth-child(' + position + ')').addClass('orc');
+    if($orc.hasClass('hero')) {
+      var damage = _.sample([1,2,3,4,5]);
+      var num = $('#health-bar > div.health').length;
+      var health = num - damage;
+      htmlUpdateHealth(health);
+    }
+    if($orc.hasClass('wormhole')){
+      console.log('Touched the Wormhole!!');
+    }
+  }
+  $pastOrcs.removeClass('pastOrc');
 }
 
 //  ------------------------------------------------------------------ //
@@ -48,13 +97,21 @@ function htmlAddBoard(game, e){
   addHealth(game);
 }
 
-function htmlAddHealth(game) {
-  for(var i = 0; i < game.hero.health; i++){
+// function htmlAddHealth(game) {
+//   for(var i = 0; i < game.hero.health; i++){
+//     var $health = $('<div>').addClass('health').attr('data-health-point', [i]);
+//     $('#health-bar').append($health);
+//   }
+// }
+
+function htmlUpdateHealth(num) {
+  $('#health-bar > div.health').remove();
+  for(var i = 0; i < num; i++){
     var $health = $('<div>').addClass('health').attr('data-health-point', [i]);
     $('#health-bar').append($health);
   }
+  var $hp = $('#health-bar > h4').text('HP: ' + $('#health-bar > div.health').length + ' pts');
 }
-
 
 //  ------------------------------------------------------------------ //
 //  ------------------------------------------------------------------ //
@@ -86,7 +143,7 @@ function addOrcs(game) {
 function addHealth(game, e) {
   var url = '/games/' + game._id + '/health';
   sendGenericAjaxRequest(url, {}, 'GET', null, e, function(data, status, jqXHR){
-    htmlAddHealth(data);
+    htmlUpdateHealth(data.hero.health);
   });
 }
 
